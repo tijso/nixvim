487e2bdd6e47b844e8da4323ef795bb4422944f3
Joey Sonnier <tijso@protonmail.com>
2024-11-13 04:50:47 -0600

    Nixvim

--- config/core/autocmd.nix
+++ config/core/autocmd.nix
@@ -1,25 +1,11 @@
 {
   autoCmd = [
-    # Vertically center document when entering insert mode
-    {
-      event = "InsertEnter";
-      command = "norm zz";
-    }
-
     # Open help in a vertical split
     {
       event = "FileType";
       pattern = "help";
       command = "wincmd L";
     }
-
-    # Close Telescope prompt in insert mode by clicking escape
-    {
-      event = [ "FileType" ];
-      pattern = "TelescopePrompt";
-      command = "inoremap <buffer><silent> <ESC> <ESC>:close!<CR>";
-    }
-
     # Enable spellcheck for some filetypes
     {
       event = "FileType";
