6a4abb7a2f497fa2c8dcb2c6b30e550e1d837f30
Joey Sonnier <tijso@protonmail.com>
2024-12-02 11:27:31 -0600

    changed theme to tokyonight

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -5,7 +5,7 @@
       options = {
         globalstatus = true;
         icons_enabled = true;
-        theme = "catppuccin";
+        theme = "tokyonight";
         disabledFileypes = {
           statusline = [
             "alpha"
