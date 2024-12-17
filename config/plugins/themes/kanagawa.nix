71597f553337ce074d8c10c44ec58f25851aa90e
Joey Sonnier <tijso@protonmail.com>
2024-12-17 12:10:59 -0600

    updated theme

--- config/plugins/themes/kanagawa.nix
+++ config/plugins/themes/kanagawa.nix
@@ -7,19 +7,30 @@
       terminal_colors = true;
       compile = true;
       style = "wave";
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none";
-            };
-          };
-        };
-      };
       styles = {
         italic = true;
         bold = true;
         transparency = true;
+      };
+      overrides = {
+        SignColumn = {
+          bg = "none";
+        };
+        FoldColumn = {
+          bg = "none";
+        };
+        LineNr = {
+          bg = "none";
+        };
+        GitSignsAdd = {
+          bg = "none";
+        };
+        GitSignsChange = {
+          bg = "none";
+        };
+        GitSignsDelete = {
+          bg = "none";
+        };
       };
     };
     package = pkgs.vimPlugins.kanagawa-nvim;
