67778eeb92c0ea7df64e360e6ff81f5d6901ff16
Joey Sonnier <tijso@protonmail.com>
2024-12-30 21:31:29 -0600

    Changed colors

--- config/plugins/themes/tokyonight.nix
+++ config/plugins/themes/tokyonight.nix
@@ -10,6 +10,23 @@
         sidebars = "transparent";
         floats = "transparent";
       };
+      colors = {
+        bg = "#011628";
+        bg_dark = "#011423";
+        bg_float = "#011423";
+        bg_highlight = "#143652";
+        bg_popup = "#011423";
+        bg_search = "#0A64AC";
+        bg_sidebar = "#011423";
+        bg_statusline = "#011423";
+        bg_visual = "#275378";
+        border = "#547998";
+        fg = "#CBE0F0";
+        fg_dark = "#B4D0E9";
+        fg_float = "#CBE0F0";
+        fg_gutter = "#627E97";
+        fg_sidebar = "#B4D0E9";
+      };
     };
     package = pkgs.vimPlugins.tokyonight-nvim;
   };
