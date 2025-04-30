60f2eec51831a6bdd0a2e8ed2f36fc4edaaa521f
Joey Sonnier <tijso@protonmail.com>
2025-04-30 12:01:48 -0500

    updated copilot plugin

--- config/plugins/cmp/cmp-copilot.nix
+++ config/plugins/cmp/cmp-copilot.nix
@@ -3,11 +3,16 @@
     enable = true;
   };
   plugins.copilot-lua = {
-    enable = true;
-    suggestion = {enabled = false;};
-    panel = {enabled = false;};
+    settings = {
+      enable = true;
+      suggestion = {
+        enabled = false;
+      };
+      panel = {
+        enabled = false;
+      };
+    };
   };
-
   extraConfigLua = ''
     require("copilot").setup({
       suggestion = { enabled = false },
