61942fa088179477c69cb68f64258890f94df4c6
Joey Sonnier <tijso@protonmail.com>
2025-06-09 13:29:01 -0500

    changed lualine theme to rode-pine

--- config/plugins/ui/lualine.nix
+++ config/plugins/ui/lualine.nix
@@ -5,7 +5,7 @@
       options = {
         globalstatus = true;
         icons_enabled = true;
-        theme = "kanagawa";
+        theme = "rose-pine";
         disabledFileypes = {
           statusline = [
             "alpha"
